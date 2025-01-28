5bdb12d4c952cd2d0e3e15ef93d6ecb170e66f3f
shishirg46 <shishirg46@gmail.com>
2025-01-28 22:11:07 +0545

    Fix path and build issues

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
